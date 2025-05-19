8f3aa3d5cf75c77915cb6695681511e6a815e528
Talmage Gaisford <07.cap-inducer@icloud.com>
2025-05-18 21:23:34 -0700

    Initial Creation of Project

---
+++ SwiftSpatchinator/SwiftSpatchinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSpatchinatorApp.swift
+//  SwiftSpatchinator
+//
+//  Created by Talmage Gaisford on 5/18/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSpatchinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
