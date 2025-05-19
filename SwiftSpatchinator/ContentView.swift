2468d1c5f6b3de1a15aed98f904c99233a50cd18
Talmage Gaisford <07.cap-inducer@icloud.com>
2025-05-18 21:41:24 -0700

    updated to skier

--- SwiftSpatchinator/ContentView.swift
+++ SwiftSpatchinator/ContentView.swift
@@ -9,11 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Text("yuh")
+            Rectangle()
+                .foregroundStyle(.blue)
+                .frame(width: 300, height: 300)
+            Image(systemName: "figure.skiing.downhill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 200, height: 200)
+                .foregroundColor(.red)
+            Text("yuh")
         }
         .padding()
     }
